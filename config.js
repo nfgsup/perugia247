57e621e6035258573b080b348f021d468ddacf96
Raffaele <sup@Servizio-RETI-WIFI.homenet.telecomitalia.it>
2026-03-30 22:11:11 +0200

    Track Firebase config for public friends demo

---
+++ config.js
@@ -0,0 +1,14 @@
+// 🔐 Configurazione Firebase
+// NOTA: Questo file contiene credenziali Firebase pubbliche
+// È aggiunto a .gitignore - NON commitarlo su GitHub pubblici!
+// Leggi solo in sviluppo locale per comodità
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDax3-gM-jSuqaT-gjUrhZpd3h8ZDbRqoY",
+  authDomain: "perugia247-d8ed2.firebaseapp.com",
+  projectId: "perugia247-d8ed2",
+  databaseURL: "https://perugia247-d8ed2-default-rtdb.europe-west1.firebasedatabase.app",
+  storageBucket: "perugia247-d8ed2.firebasestorage.app",
+  messagingSenderId: "684669568391",
+  appId: "1:684669568391:web:f03c9dda51c17188647f22"
+};
